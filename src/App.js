ea516df7447da7628d87ed8ae8c76da7561bc36b
BasLightyear <baskleisen@gmail.com>
2019-09-17 16:44:42 +0200

    Learning things

--- src/App.js
+++ src/App.js
@@ -1,60 +1,157 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org",
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const DEFAULT_QUERY = "redux"
+const DEFAULT_HPP = "100"
 
-class Developer {
-  constructor(firstname, lastname){
-    this.firstname = firstname
-    this.lastname = lastname
-  }
+const PATH_BASE = "https://hn.algolia.com/api/v1"
+const PATH_SEARCH = "/search"
+const PARAM_SEARCH = "query="
+const PARAM_PAGE = "page="
+const PARAM_HPP = "hitsPerPage="
 
-  getName(){
-    return `${this.firstName} ${this.lastName}`
-  }
+const Search = ({ value, onChange, onSubmit, children }) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
+      <button type="submit">
+        {children}
+      </button>
+    </form>
+  )
 }
 
-const basLightyear = new Developer("Bas", "Lightyear")
+const Button = ({ onClick, className, children }) => {
+  return(
+    <button
+      onClick={onClick}
+      className={className}
+      type="button"
+    >
+      {children}
+    </button>
+  )
+}
+
+const Table = ({ list, onDismiss }) => {
+    return (
+      <div className="table">
+        {list.map(item =>
+              <div key={item.objectID} className="table-row">
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+                <span>
+                  <Button
+                    onClick={() => onDismiss(item.objectID)}
+                    className="button-inline"
+                  >
+                    Dismiss
+                  </Button>
+                </span>
+              </div>
+            )}
+      </div>
+    )
+}
 
 class App extends Component {
+  constructor(props){
+    // Calling constructor stuff from parent class
+    super(props)
+
+    // Setting the initial state
+    this.state = {
+      result: null,
+      searchTerm: DEFAULT_QUERY,
+    }
+    
+    // Binding functions
+    this.setSearchTopStories = this.setSearchTopStories.bind(this)
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this)
+    this.onSearchChange = this.onSearchChange.bind(this)
+    this.onSearchSubmit = this.onSearchSubmit.bind(this)
+    this.onDismiss = this.onDismiss.bind(this)
+  }
+
+  setSearchTopStories(result){
+    const { hits, page } = result
+    
+    const oldHits = page !== 0 ? this.state.result.hits : []
+    const updatedHits = [ ...oldHits, ...hits ]
+
+    this.setState({
+      result: { hits: updatedHits, page }
+    })
+  }
+  
+  fetchSearchTopStories(searchTerm, page = 0){
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error)
+  }
+
+  componentDidMount(){
+    const { searchTerm } = this.state
+    this.fetchSearchTopStories(searchTerm)
+  }
+
+  onSearchSubmit(event){
+    const { searchTerm } = this.state
+    this.fetchSearchTopStories(searchTerm)
+    event.preventDefault()
+  }
+
+  onSearchChange(event){
+    this.setState({ searchTerm: event.target.value })
+  }
+
+  onDismiss(id){
+    const updatedHits = this.state.result.hits.filter(item => item.objectID !== id)
+    this.setState({ 
+      result: { ...this.state.result, hits: updatedHits }
+    })
+  }
+
   render() {
+    const { searchTerm, result } = this.state
+    const page = (result && result.page)  || 0
+
+    if (!result) { return null }
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React, {basLightyear.firstname} {basLightyear.lastname}. You handsome devil!</h2>
+      <div className="page">
+        <div className="interactions">
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
+          >
+            Search
+          </Search>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        {list.map(item => 
-              <div>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </div>
-        )}
+        { result
+          ?
+          <Table
+            list={result.hits}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}
+          />
+          : null
+        }
+        <div className="interactions">
+          <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
+            More
+          </Button>
+        </div>
       </div>
     );
   }
